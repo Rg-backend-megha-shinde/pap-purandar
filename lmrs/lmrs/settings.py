815b2a72fcbd6f52b510a1acb506e8fd5db9d657
Ankush Jadhav <ankushjadhavrg1@gmail.com>
2026-03-18 16:13:48 +0530

    settings.py fixed

--- lmrs/lmrs/settings.py
+++ lmrs/lmrs/settings.py
@@ -59,7 +59,6 @@ ROOT_URLCONF = 'lmrs.urls'
 
 TEMPLATES = [
     { 
-    { 
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
